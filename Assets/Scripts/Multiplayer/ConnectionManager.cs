9368ca6a188d302b0ce4a44aa1f00f82e8eed611
Shubham Tyagi <shubham@onerare.io>
2022-12-27 12:36:14 +0530

    Updated Connection manager

--- Assets/Scripts/Multiplayer/ConnectionManager.cs
+++ Assets/Scripts/Multiplayer/ConnectionManager.cs
@@ -6,12 +6,18 @@ public class ConnectionManager : MonoBehaviour
 {
     [SerializeField] private Server server = Server.LocalHost;
     [SerializeField] private Connection connection = Connection.None;
-    [SerializeField] private string awsIp = "43.204.232.118";
-    [SerializeField] private string doceanIp = "142.93.211.48";
     [SerializeField] private Bayou bayou;
+    private string ip_aws = "43.204.232.118";
+    private string ip_digitalocean_live = "142.93.211.48";
+    private string ip_digitalocean_dev = "146.190.35.150";
+
+    [Header("-----DEBUG-----")]
+    [SerializeField] private string IP;
+    [SerializeField] private ushort PORT;
+    [SerializeField] private string CONNECTION;
 
 
-    public enum Server { LocalHost, DigitalOcean, AWS };
+    public enum Server { LocalHost, DigitalOcean_Dev, DigitalOcean_Live, AWS };
     public enum Connection { Client, Server, Both, None };
 
     private void Awake() => bayou = InstanceFinder.NetworkManager.gameObject.GetComponent<Bayou>();
@@ -25,42 +31,61 @@ public class ConnectionManager : MonoBehaviour
         switch (server)
         {
             case Server.LocalHost:
-                bayou.SetPort(8080);
-                bayou.SetClientAddress("localhost");
+                PORT = 8080;
+                IP = "localhost";
                 break;
-            case Server.DigitalOcean:
-                bayou.SetPort(8000);
-                bayou.SetClientAddress(doceanIp);
+
+            case Server.DigitalOcean_Dev:
+                PORT = 8800;
+                IP = ip_digitalocean_dev;
                 break;
+
+            case Server.DigitalOcean_Live:
+                PORT = 8800;
+                IP = ip_digitalocean_live;
+                break;
+
             case Server.AWS:
-                bayou.SetPort(8000);
-                bayou.SetClientAddress(awsIp);
+                PORT = 8000;
+                IP = ip_aws;
                 break;
+
             default:
-                bayou.SetPort(8080);
-                bayou.SetClientAddress("localhost");
+                PORT = 8080;
+                IP = "localhost";
                 break;
         }
+        bayou.SetPort(PORT);
+        bayou.SetClientAddress(IP);
+
+
 
         // Mode Setup
         switch (connection)
         {
             case Connection.Client:
+                CONNECTION = "Client";
                 InstanceFinder.ClientManager.StartConnection();
                 break;
+
             case Connection.Server:
+                CONNECTION = "Server";
                 InstanceFinder.ServerManager.StartConnection();
                 break;
+
             case Connection.Both:
+                CONNECTION = "Both";
                 InstanceFinder.ServerManager.StartConnection();
                 InstanceFinder.ClientManager.StartConnection();
                 break;
+
             case Connection.None:
-                InstanceFinder.ServerManager.StartConnection();
-                InstanceFinder.ClientManager.StartConnection();
+                CONNECTION = "NONE";
                 break;
             default:
                 break;
         }
+
+        print($"PORT -> {PORT}  |  IP -> {IP}  |  SERVER -> {CONNECTION}");
     }
 }
